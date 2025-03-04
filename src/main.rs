c9c2c8416538b8f16b55d9488fa1ec161d093265
Suharjin <suharjin01@gmail.com>
2025-03-04 09:16:28 +0800

    Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use validator::Validate;
+
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[derive(Debug, Validate)]
+struct LoginRequest {
+    #[validate(length(min = 3, max = 15))]
+    username: String,
+    #[validate(length(min = 3, max = 20))]
+    password: String,
+}
+
+// contoh untuk validasi berhasil
+#[test]
+fn test_validate_success() {
+    let login = LoginRequest {
+        username: "suharjin07".to_string(),
+        password: "12345".to_string()
+    };
+
+    assert!(login.validate().is_ok())
+}
+
+// contoh untuk validasi gagal
+#[test]
+fn test_validate_failed() {
+    let login = LoginRequest {
+        username: "c1".to_string(),
+        password: "12".to_string()
+    };
+
+    assert!(login.validate().is_err());
+
+    let error = login.validate().err().unwrap();
+    println!("{:?}", error.errors());
 }
